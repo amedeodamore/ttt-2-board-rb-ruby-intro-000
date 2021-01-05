89edbb703d190bf10221abea839d912d40de7090
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-05 00:54:41 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,19 @@ board = "board"
 
 board = [" "," "," "," "," "," "," "," "," "]
 
+# Position 1: Left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+# Position 3: Right
+row[2]
+
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]
+
 
 #def board
 #  board = [" "," "," "," "," "," "," "," "," "]
